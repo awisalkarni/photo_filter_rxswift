0a8ffc976737a1a26e48f8189e278041bc7a5d18
awis alkarni <awis@awislabs.com>
2021-03-20 15:05:07 +0800

    initial commit

---
+++ Camera Filter/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Camera Filter
+//
+//  Created by Awis Alkarni on 19/03/2021.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+}
